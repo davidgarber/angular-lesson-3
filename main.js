300d777718272854f8d524945c476d5b7284d682
David Garber <davidgarber@gmail.com>
2015-12-09 14:47:40 -0800

    Add .post to endpoints.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 var app = angular.module('minmax', []);
 
+app.controller('MinMaxCtrl', function ($scope, $http) {
+  $scope.formModel = {};
+
+  $scope.onSubmit = function () {
+    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+    success(function (data) {
+      console.log("success")
+    }).error(function (data) {
+      console.log("error")
+    });
+  };
+})
+
 // https://minmax-server.herokuapp.com/register/'
